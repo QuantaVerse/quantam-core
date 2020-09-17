00a1ff180ca6269a69b9dc76d392f39f34f17ace
Pritam <pritam.sarkar@medlife.com>
2020-09-17 12:07:12 +0530

    Add data retriver interface

--- src/retriever/qd.retriever.service.ts
+++ src/retriever/qd.retriever.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@nestjs/common";
+import {QuantamDataRetrieverServiceInterface} from "./interfaces/qd.retriever.interface";
 
 @Injectable()
-export class QuantamDataRetrieverService {
+export class QuantamDataRetrieverService implements QuantamDataRetrieverServiceInterface {
     getHealth(): Record<string, string> {
         return {
             status: "OK",
