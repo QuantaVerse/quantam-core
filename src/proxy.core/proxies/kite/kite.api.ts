733d19361217cb74922d604da66f70a364bdbf99
Pritam <pritam.sarkar@medlife.com>
2020-09-20 19:26:50 +0530

    Modify KiteConnect

--- src/proxy.core/proxies/kite/kite.api.ts
+++ src/proxy.core/proxies/kite/kite.api.ts
@@ -1,25 +1,29 @@
-// import { KiteConnect } from "kiteconnect";
-//
-// export class KiteApi {
-//     private kc = new KiteConnect({
-//         api_key: "your_api_key"
-//     });
-//
-//     kc.generateSession("request_token", "api_secret").then(function(response) {
-//         init();
-//     }).catch(function(err) {
-//         console.log(err);
-//     });
-//
-//     function init() {
-//         // Fetch equity margins.
-//         // You can have other api calls here.
-//         kc.getMargins()
-//             .then(function(response) {
-//                 // You got user's margin details.
-//             }).catch(function(err) {
-//             // Something went wrong.
-//         });
-//     }
-//
-// }
+import { KiteConnect } from "kiteconnect";
+
+export class KiteApi {
+    private readonly kc;
+
+    constructor(kc: KiteConnect) {
+        this.kc = new KiteConnect({ api_key: "your_api_key" });
+    }
+    // kc = new KiteConnect({
+    //     api_key: "your_api_key"
+    // });
+    //
+    // kc.generateSession("request_token", "api_secret").then(function(response) {
+    //     init();
+    // }).catch(function(err) {
+    //     console.log(err);
+    // });
+    //
+    // function init() {
+    //     // Fetch equity margins.
+    //     // You can have other api calls here.
+    //     kc.getMargins()
+    //         .then(function(response) {
+    //             // You got user's margin details.
+    //         }).catch(function(err) {
+    //         // Something went wrong.
+    //     });
+    // }
+}
